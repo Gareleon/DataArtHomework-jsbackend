6eefa90508304972d81c16c99e60d0740ab7c897
Gareleon <dragan.ignjatovic.student@gmail.com>
2025-02-19 23:31:48 +0100

    feat: defined HTML to serve on server homepage

---
+++ src/public/index.js
@@ -0,0 +1,42 @@
+export const Message = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Jokes API</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #1f2937;
+        color: white;
+        text-align: center;
+      }
+      .footer {
+        background-color: #1f2937;
+        padding: 20px;
+        font-weight: lighter;
+      }
+      .footer p {
+        margin: 0;
+      }
+      .footer a {
+        color: #38bdf8;
+        text-decoration: none;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="footer">
+      <h1>Jokes Vote Server</h1>
+      <p>
+        Made by
+        <a href="https://www.draganignjatovic.com" class="text-primary"
+          >Dragan Ignjatovic</a
+        >
+      </p>
+    </div>
+  </body>
+</html>
+`;
